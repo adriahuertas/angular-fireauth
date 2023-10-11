87c438d82d4e1f8edc969663b0dfa1febef8f95f
Adrià Huertas <adria.huertas@aetech.biz>
2023-10-11 09:19:24 +0200

    Added firebase library

---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    projectId: 'angular-auth-c46dd',
+    appId: '1:688139786284:web:f9d46c895cf8fa494fd989',
+    storageBucket: 'angular-auth-c46dd.appspot.com',
+    apiKey: 'AIzaSyDzIskyOYfV8IWzfuXJLq4hS2f9l4GHswA',
+    authDomain: 'angular-auth-c46dd.firebaseapp.com',
+    messagingSenderId: '688139786284',
+  },
+};
